f3f12f1a037b228e0c6de790e28afcb10b4af088
Ajitesh Channa <ac.ajiteshchanna@gmail.com>
2026-02-09 12:36:37 +0530

    adding MissingNumber.java

---
+++ Arrays/MissungNumber.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class MissungNumber {
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int checker = 0;
+        Arrays.sort(nums);
+
+        for(int i = 0;i<n;i++){
+            if(nums[i] != checker){
+                return checker;
+            }
+            checker++;
+        }
+        return checker;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
