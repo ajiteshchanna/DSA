a85eb67e7e61035959ce330e9eccec6063934612
Ajitesh Channa <ac.ajiteshchanna@gmail.com>
2026-03-02 19:05:32 +0530

    adding Reverse Only Letters Solution

---
+++ Strings/ReverseOnlyLetters.java
@@ -0,0 +1,63 @@
+package Strings;
+
+public class ReverseOnlyLetters {
+
+    public static void printArr(String[] arr){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i] +" , ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        String s = "Test1ng-Leet=code-Q!";
+        System.out.println(s);
+        String res = reverseLetters(s);
+        System.out.println(res);
+    }
+
+    public static String reverseLetters(String s){
+        // creating an array so that swapping will be inplace and O(1)
+        String []arr = s.split("");
+
+        // we will be using two pointer approach
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i<=j){
+            char a = s.charAt(i);
+            char b = s.charAt(j);
+
+            boolean aLetter = (a>=65 && a<=90) || (a>=97 && a<=122);
+            boolean bLetter = (b>=65 && b<=90) || (b>=97 && b<=122);
+
+            // only letter
+            if(aLetter && bLetter){
+                // swap
+                arr[i] = Character.toString(b);
+                arr[j] = Character.toString(a);
+
+                i++;
+                j--;
+            } else if(aLetter && !bLetter){
+                // a letter but b not letter
+                j--;
+            } else if(bLetter && !aLetter){
+                // b letter but a not letter
+                i++;
+            } else {
+                // both not letter
+                i++;
+                j--;
+            }
+        }
+
+        printArr(arr);
+        StringBuilder sb = new StringBuilder("");
+        for(int k=0;k<arr.length;k++){
+            sb.append(arr[k]);
+        }
+
+        return sb.toString();
+    }
+}
--- Strings/UncommonWords.java
+++ Strings/UncommonWords.java
@@ -21,11 +21,14 @@ public class UncommonWords{
     }
 
     public static String[] findUncommonWords(String s1, String s2){
+        // creating arrays for better traversals
         String arr1[] = s1.split(" ");
         String arr2[] = s2.split(" ");
 
+        // an ordered hashmap to store string,freuency in pair
         LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
 
+        // filling map
         for(String str:arr1){
             if(map.containsKey(str)){
                 map.put(str, map.get(str)+1);
@@ -42,10 +45,15 @@ public class UncommonWords{
             }
         }
 
+        // list to store the uncommon (unique) words
         ArrayList<String> list = new ArrayList<>();
+
+        // finding the unique words
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             String key = entry.getKey();
             Integer value = entry.getValue();
+
+            // if freq == 1, then its unique
             if(value == 1){
                 list.add(key);
             }
