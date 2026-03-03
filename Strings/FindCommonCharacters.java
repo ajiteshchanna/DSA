2d3132ab347a05254c9ee3b5ee564114d7eeaea9
Ajitesh Channa <ac.ajiteshchanna@gmail.com>
2026-03-03 21:21:01 +0530

    updating files

--- Strings/FindCommonCharacters.java
+++ Strings/FindCommonCharacters.java
@@ -1,50 +1,56 @@
-package Strings;
 import java.util.*;
 
-public class FindCommonCharacters{
+public class FindCommonCharacters {
 
-    public static void fillCount(String word, int count[]){
-        for(int i = 0;i<word.length();i++){
-            count[word.charAt(i)-'a']++;
+    // Function to count frequency of characters in a word
+    public static void fillCount(String word, int[] count) {
+        for (int i = 0; i < word.length(); i++) {
+            // Increment frequency of current character
+            count[word.charAt(i) - 'a']++;
         }
     }
-    public static void main(String args[]) {
-        String []words = {"cook","lock","cool"};
-        System.out.println(commonChars(words));
-    }
 
-    public static List<String> commonChars(String[] words){
-        ArrayList<String> list = new ArrayList<>();
-        int n = words.length;
+    public static List<String> commonChars(String[] words) {
 
-        int count[] = new int[26];
-        for(int i = 0;i<count.length;i++){
-            count[i] = 0;
-        }
+        List<String> result = new ArrayList<>();
 
+        // Frequency array for storing minimum common frequency
+        int[] count = new int[26];
+
+        // Step 1: Initialize frequency using first word
         fillCount(words[0], count);
 
-        for(int i = 1;i<words.length;i++){
-            int temp[] = new int[26];
-            for(int j = 0;j<26;j++){
-                temp[j] = 0;
-            }
+        // Step 2: Compare with remaining words
+        for (int i = 1; i < words.length; i++) {
 
+            // Temporary frequency array for current word
+            int[] temp = new int[26];
+
+            // Count frequency of current word
             fillCount(words[i], temp);
 
-            for(int j = 0;j<26;j++){
+            // Update main count array with minimum frequency
+            // This keeps only common characters
+            for (int j = 0; j < 26; j++) {
                 count[j] = Math.min(count[j], temp[j]);
             }
         }
 
-        for(int i = 0;i<26;i++){
-            int c = count[i];
-            while(c>0){
-                list.add(Character.toString(i+'a'));
-                c--;
+        // Step 3: Build result from final frequency array
+        for (int i = 0; i < 26; i++) {
+
+            // Add character i ('a' + i) count[i] times
+            while (count[i] > 0) {
+                result.add(Character.toString((char) (i + 'a')));
+                count[i]--;
             }
         }
 
-        return list;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"cook", "lock", "cool"};
+        System.out.println(commonChars(words));
     }
 }
